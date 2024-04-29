b2498e7762d85b8ee42866062c73392753c2fb78
Sejusa <110556472+Sejusa@users.noreply.github.com>
2024-04-29 09:43:40 +0200

    V 1.3.4

--- Bari_3800.ino
+++ Bari_3800.ino
@@ -9,6 +9,7 @@ int button_right = 11;
 int button_left = 12;
 int button_on = 6;
 int on;
+int LastButtonPress = 0;
 
 // Variables will change:
 int buttonState = LOW;            // the current reading from the input pin
@@ -19,10 +20,16 @@ int lastButtonState = LOW;  // the previous reading from the input pin
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 70;    // the debounce time; increase if the output flickers
 
-bool motorRunning = false; // Variable para rastrear si el motor está girando o no
+bool motorRunning = false; // Variable para rastrear si el motor está girando o no.
 
-void setup()
-{
+int forward;
+int backward;
+int right;
+int left;
+
+int button;
+void setup() {
+  
   pinMode(activate, OUTPUT);
   pinMode(right_one, OUTPUT);
   pinMode(right_two, OUTPUT);
@@ -33,47 +40,49 @@ void setup()
   pinMode(button_right, INPUT);
   pinMode(button_left, INPUT);
   pinMode(button_on, INPUT);
-  Serial.begin(9600); //Para iniciar la comunicación con la consola.
+  Serial.begin(9600); //Para iniciar la comunicación con la consola.:
+
 }
 
-void loop()
-{ 
-  turn_on();
+void loop() {
+
+   actual_Button();
+
 }
 
-void move(char mov)
+void move(int button)
 {
-  switch(mov)
+  switch(button)
   {
-    case 'F': //Movimiento para adelante.
+    case 1: //Movimiento para adelante.
       digitalWrite(right_one, HIGH);
       digitalWrite(left_one, HIGH);
       digitalWrite(right_two, LOW);
       digitalWrite(left_two, LOW);
       break;
 
-    case 'B': //Movimiento hacia atrás.
+    case 2: //Movimiento hacia atrás.
         digitalWrite(right_one, LOW);
         digitalWrite(left_one, LOW);
         digitalWrite(right_two, HIGH);
         digitalWrite(left_two, HIGH);
         break;
 
-      case 'L': //Movimiento hacia la izquierda. Hacemos girar la rueda derecha pero no la izquierda para girar.
+      case 3: //Movimiento hacia la izquierda. Hacemos girar la rueda derecha pero no la izquierda para girar.
         digitalWrite(right_one, LOW);
         digitalWrite(left_one, HIGH);
         digitalWrite(right_two, LOW);
         digitalWrite(left_two, LOW);
         break;
 
-      case 'R': //Movimiento hacia la derecha. Hacemos girar la rueda izquierda pero no la derecha para girar.
+      case 4: //Movimiento hacia la derecha. Hacemos girar la rueda izquierda pero no la derecha para girar.
         digitalWrite(right_one, HIGH);
         digitalWrite(left_one, LOW);
         digitalWrite(right_two, LOW);
         digitalWrite(left_two, LOW);
         break;
       
-      case 'S': //Para parar las ruedas.
+      case 0: //Para parar las ruedas.
         digitalWrite(right_one, LOW);
         digitalWrite(left_one, LOW);
         digitalWrite(right_two, LOW);
@@ -81,41 +90,81 @@ void move(char mov)
   }
 }
 
-void turn_on() { 
-    int reading = digitalRead(button_on);
-    on = LOW;
+void turn_On(int pin, int button) //'pin' es el pin donde esta conectado el botón y 'button' lo usaremos para controlar la variable 'move'.
+{ 
+    int reading = digitalRead(pin);
+    if (reading == HIGH)
+    {    
+    int x = pin - LastButtonPress; //Para establecer el valor de 'x'.
+        if (x == 0)
+        {
+          move(button);
+        }
 
-    if (reading != lastButtonState) //Comprueba si el estado leído del botón es diferente al último estado guardado (lastButtonState).
-    {
+        else 
+        {
         lastDebounceTime = millis(); //Si hay un cambio en el estado del botón, actualiza el tiempo del último rebote (debounce) con el tiempo actual en milisegundos.
-    }
-
-    if ((millis() - lastDebounceTime) > debounceDelay) // Comprueba si ha pasado el tiempo de rebote (debounceDelay) desde el último rebote.
-    {
-      if (reading != buttonState) {
-          buttonState = reading; //Si hay un cambio en el estado del botón, se actualiza la variable buttonState con el nuevo estado leído del botón. 
-
-          if (buttonState == HIGH) 
-          {
-              on = !on; // Invierte el estado de la variable on. Si on es true, se convierte en false, y viceversa. 
-          }
+           
+           if ((millis() - lastDebounceTime) > debounceDelay) // Comprueba si ha pasado el tiempo de rebote (debounceDelay) desde el último rebote.
+           { 
+              x = 0; //Establecemos el valor de 'x' a 0.
+           }   
         }
     }
 
-    if (on == HIGH) // Si el botón está presionado
+    else
     {
-      if (motorRunning) // Si las ruedas ya están girando
-      { 
-          move('S'); // Detener las ruedas
-          motorRunning = false;
-      }
-
-      else 
-      {
-          move('R'); // Girar a la derecha
-          motorRunning = true;
-      }
+     // código futuro
     }
+    
+  LastButtonPress = pin;
+}
 
-    lastButtonState = reading; //Actualiza el último estado del botón con el estado leído actual para usarlo en la próxima iteración de la función.
+void actual_Button()
+{
+  forward = digitalRead(button_forward);
+  backward = digitalRead(button_back);
+  right = digitalRead(button_right);
+  left = digitalRead(button_left);
+
+  if(forward == 0 && backward == 0 && right == 0 && left == 0)
+  {
+    button = 0;
+   // Serial.println("Quieto");
+    move(button);
+   // turn_On(button);
+  }
+
+  if(forward == 0 && backward == 0 && right == 0 && left == 1)
+  {
+    button = 3;
+    //Serial.println("Izquierda");
+    //move(button);
+    turn_On(button_left, button);
+    
+  }
+
+  if(forward == 0 && backward == 0 && right == 1 && left == 0)
+  {
+    button = 4;
+    //Serial.println("Derecha");
+    //move(button);
+    turn_On(button_right, button);
+  }
+
+  if(forward == 0 && backward == 1 && right == 0 && left == 0)
+  {
+    button = 2;
+    //Serial.println("Atras");
+    //move(button);
+    turn_On(button_back, button);
+  }
+
+  if(forward == 1 && backward == 0 && right == 0 && left == 0)
+  {
+    button = 1;
+    //Serial.println("Delante");
+    //move(button);
+    turn_On(button_forward, button);
+  }
 }
